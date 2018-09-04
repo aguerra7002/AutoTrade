a37da97ef801ac758a0c3904e94f688521a1f773
Alex Guerra <alexguerra618@gmail.com>
2018-09-04 02:31:22 -0400

    Added other main class to be distinct from Test.

---
+++ Binance_Trader/src/API/TradeRun.java
@@ -0,0 +1,36 @@
+package API;
+
+import actions.MarketFetchAction;
+import actions.SetupAction;
+import balance.BalanceHub;
+import server.WebServer;
+import traders.MLSinewaveFitRidgeDetectorTrader;
+
+
+
+/**
+ * Main Class where we run the Trader from. Note that we can test in the Test class.
+ * @author alexg
+ */
+public class TradeRun {
+
+
+	public static void main(String[] args) {
+		SetupAction sa = new SetupAction(Constants.BTC_USDT_MARKET_SYMBOL);
+		sa.getMinQty();
+		MarketFetchAction mfa = new
+		MarketFetchAction(Constants.BTC_USDT_MARKET_SYMBOL, 1);
+		System.out.println("Starting trading... Start Price: " +
+		mfa.getCurrentPrice());
+		BalanceHub hub = BalanceHub.getInstance();
+		hub.setValue(1000d, 0d);
+		WebServer server = new WebServer();
+		server.startServer();
+		MLSinewaveFitRidgeDetectorTrader trader = new
+		MLSinewaveFitRidgeDetectorTrader(true);
+
+		trader.begin();
+
+	}
+
+}
